20dac7517930672f93f6c3b5061b052376e3feda
Thomas Hingre <thomas.hingre@schiller.fr>
2025-02-13 11:33:03 +0100

    feat: add general user env var
    Signed-off-by: Thomas Hingre <thomas.hingre@schiller.fr>

--- init.lua
+++ init.lua
@@ -1053,5 +1053,5 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
---
+vim.g.user = os.getenv("USER") or os.getenv("USERNAME")
 require 'custom.keymaps'
