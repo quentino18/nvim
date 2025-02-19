d3989e38ad4ea8e682f9a190820323c1ca38e75a
Thomas Hingre <thomas.hingre@schiller.fr>
2025-02-19 15:03:08 +0100

    feat: add tagbar plugins on F12 key
    Signed-off-by: Thomas Hingre <thomas.hingre@schiller.fr>

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,27 +5,32 @@
 ---@module 'lazy'
 ---@type LazySpec
 return {
-  { 'wsdjeg/vim-fetch' },
-  { 'farmergreg/vim-lastplace' },
-  {
-    'lfv89/vim-interestingwords',
-    opts = {},
-    config = function() vim.g.interestingWordsGUIColors = { '#2E56DB', '#8CCBEA', '#A4E57E', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF' } end,
-  },
-  {
-    'lambdalisue/suda.vim',
-    opts = {},
-    config = function() vim.api.nvim_create_user_command('W', 'SudaWrite', { desc = 'Write as sudoer' }) end,
-  },
-  {
-    'iberianpig/tig-explorer.vim',
-    config = function()
-      vim.keymap.set('n', '<leader>ht', function()
-        -- Rediriger la sortie pour éviter les messages affichés par Neovim
-        vim.cmd 'silent! call feedkeys(":TigBlame\\<CR>", "n")'
-      end, { desc = 'git [t]ig blame' })
-    end,
-  },
-  { 'mbbill/undotree' },
-  { 'powerman/vim-plugin-AnsiEsc' },
+	{	'wsdjeg/vim-fetch'},
+	{	'farmergreg/vim-lastplace'},
+	{
+		'lfv89/vim-interestingwords',
+		opts = {},
+		config = function()
+			vim.g.interestingWordsGUIColors = {'#2E56DB', '#8CCBEA', '#A4E57E', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF'}
+		end
+	},
+	{
+		'lambdalisue/suda.vim',
+		opts = {},
+		config = function()
+			vim.api.nvim_create_user_command('W', 'SudaWrite', { desc = "Write as sudoer"})
+		end
+	},
+	{
+		'iberianpig/tig-explorer.vim',
+		config = function()
+			vim.keymap.set('n', '<leader>ht', function()
+				-- Rediriger la sortie pour éviter les messages affichés par Neovim
+				vim.cmd('silent! call feedkeys(":TigBlame\\<CR>", "n")')
+			end, { desc = 'git [t]ig blame' })
+		end,
+	},
+	{	'preservim/tagbar'},
+	{	'mbbill/undotree'},
+	{	'powerman/vim-plugin-AnsiEsc'},
 }
