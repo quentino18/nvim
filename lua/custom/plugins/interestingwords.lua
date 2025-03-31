b0df034b617c6bfa3d816916df490b940bfa74ad
Quentin Colman <quentin.colman@schiller.fr>
2025-03-31 10:13:49 +0200

    feat: Use ririck0 repo to apply vim-interestingwords colors to all windows
    Signed-off-by: Quentin Colman <quentin.colman@schiller.fr>

--- lua/custom/plugins/interestingwords.lua
+++ lua/custom/plugins/interestingwords.lua
@@ -1,9 +1,9 @@
 return {
 	{
-		'lfv89/vim-interestingwords',
+		'ririck0/vim-interestingwords',
 		opts = {},
 		config = function()
-			vim.g.interestingWordsGUIColors = {'#2E56DB', '#8CCBEA', '#A4E57E', '#FFDB72', '#FF7272', '#FFB3FF', '#9999FF'}
+			vim.g.interestingWordsGUIColors = {'#F7C873', '#8BC34A', '#00BCD4', '#FF5722', '#E91E63', '#4CAF50', '#FFEB3B', '#CDDC39', '#03A9F4', '#9C27B0', '#673AB7', '#FFC107', '#2196F3', '#795548', '#F44336', '#FF9800', '#3F51B5', '#00E676', '#FFEB3B', '#FFB74D', '#FF4081', '#8E24AA', '#00ACC1', '#00796B', '#64B5F6', '#AED581', '#B39DDB', '#D4E157', '#FF7043', '#BA68C8', '#81C784', '#F06292'}
 		end
 	},
 }
