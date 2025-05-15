8e0e04c0b9f906629966b12be76c9a0c92ea626c
Quentin Colman <quentin.colman@schiller.fr>
2025-05-15 15:37:42 +0200

    feat: Add '<leader>d' and '<leader>D' for Definition/Declaration
    Neovim 0.11 introduced new LSP binding with 'gr*' like 'grd', 'grD', ...
    It's a a bit cumbersome to do ...
    So let's ease the most used ones.
    
    NOTE: To return from a 'Definition' after 'grd' : <Ctrl-t>
    
    Signed-off-by: Quentin Colman <quentin.colman@schiller.fr>

--- init.lua
+++ init.lua
@@ -572,9 +572,23 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('gra', vim.lsp.buf.code_action, '[G]oto Code [A]ction', { 'n', 'x' })
 
+          -- Find references for the word under your cursor.
+          map('grr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+
+          -- Jump to the implementation of the word under your cursor.
+          --  Useful when your language has ways of declaring types without an actual implementation.
+          map('gri', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+
+          -- Jump to the definition of the word under your cursor.
+          --  This is where a variable was first declared, or where a function is defined, etc.
+          --  To jump back, press <C-t>.
+          map('grd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('<leader>d', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+          map('<leader>D', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
