41993a9e6fef7278dc216c531efd6aa1cfefd557
Quentin Colman <quentin.colman@schiller.fr>
2025-08-26 11:56:31 +0200

    fix: make '<leader>D' go to Declaration instead of Definition
    Signed-off-by: Quentin Colman <quentin.colman@schiller.fr>

--- init.lua
+++ init.lua
@@ -588,7 +588,7 @@ require('lazy').setup({
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('grD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-          map('<leader>D', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('<leader>D', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
