af1ddf89b61d862cfedb1daf8f00a782dacebe34
Quentin Colman <quentin.colman@schiller.fr>
2026-04-01 13:09:09 +0200

    fix: Remove duplicate stylua
    Signed-off-by: Quentin Colman <quentin.colman@schiller.fr>

--- init.lua
+++ init.lua
@@ -662,7 +662,6 @@ require('lazy').setup({
         hadolint = {},
         pylint = {},
         shellcheck = {},
-        stylua = {},
         ts_ls = {},
         yamllint = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
